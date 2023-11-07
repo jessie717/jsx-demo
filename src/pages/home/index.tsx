96750967dcb6b671aa659d140f4fc480f6c09e51
jessie <jessiezly717@gmail.com>
2023-11-07 23:49:21 +0800

    feat: 删除冗余代码、重写home页逻辑

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,52 @@
+import { defineComponent } from 'vue'
+import { ElButton, ElCheckbox } from 'element-plus'
+import MyTable from '../../components/myTable'
+
+import { tableData, columns } from './constants'
+
+export default defineComponent({
+	name: 'Home',
+	components: { ElButton, ElCheckbox },
+	setup() {
+		const handleCheck = (id: string) => {
+			console.log('check id', id)
+		}
+		const handleDelete = (row: TableDataItem) => {
+			console.log('row', row)
+		}
+		// const slots = {
+		// 	operation: (row: TableDataItem) => (
+		// 		<>
+		// 			<el-button type="success" onClick={() => handleCheck(row.id)}>
+		// 				查看
+		// 			</el-button>
+		// 			<el-button type="danger" onClick={() => handleDelete(row)}>
+		// 				删除
+		// 			</el-button>
+		// 		</>
+		// 	)
+		// }
+		return () => (
+			<>
+				<div class="w-screen h-screen bg-white">
+					{/* <MyTable tableData={tableData} columns={columns} v-slots={slots}></MyTable> */}
+					<MyTable tableData={tableData} columns={columns}>
+						{{
+							operation: (row: TableDataItem) => (
+								<>
+									<el-button type="success" onClick={() => handleCheck(row.id)}>
+										查看
+									</el-button>
+									<el-button type="danger" onClick={() => handleDelete(row)}>
+										删除
+									</el-button>
+								</>
+							),
+							// selection: () => <el-checkbox />
+						}}
+					</MyTable>
+				</div>
+			</>
+		)
+	}
+})
